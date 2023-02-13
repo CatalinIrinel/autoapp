976917f87c503167151180d8bd5d1b16eb00908b
CatalinIrinel <catalin.istr22@gmail.com>
2023-02-13 23:25:23 +0200

    new-app

---
+++ src/components/home/Hero.jsx
@@ -0,0 +1,21 @@
+import { Box, Flex, Heading, Stack } from '@chakra-ui/react';
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <Flex
+      w={'full'}
+      h={'100vh'}
+      flexDir={'column'}
+      alignItems={'center'}
+      justifyContent={'center'}
+    >
+      <Heading as={'h1'}>
+        Un mod usor si rapid de a posta pe social media
+      </Heading>
+      <Stack></Stack>
+    </Flex>
+  );
+};
+
+export default Hero;
